e02aefecc088c9c4fd2fd42171b37e29f683b71f
hyangsh <hyangsh92@gmail.com>
2025-10-26 15:29:25 +0900

    식사 조합 추천 기능 추가 + 기존 애플리케이션과 통합

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import InventoryPage from './components/InventoryPage';
+
+function App() {
+  return (
+    <div className="App">
+      <InventoryPage />
+    </div>
+  );
+}
+
+export default App;
--- student-meal-planner/backend/src/routes/recommendationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMealRecommendations } = require('../controllers/recommendationController');
-
-// This route should be protected
-router.get('/meals', getMealRecommendations);
-
-module.exports = router;
